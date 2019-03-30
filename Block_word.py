23c2ca36687ff2ce46e7a6e892426c29e413920d
LOHITH <35166080+lohith9@users.noreply.github.com>
2019-03-30 21:57:32 +0530

    Create Block_word.py

---
+++ Block_word.py
@@ -0,0 +1,19 @@
+g=['D','C','B','A']
+i=["A","B","C","D"]
+cl=[]
+t=0
+le=len(i)
+for i in range(le):
+    le=i.pop(i)
+    i.append(k)
+for k in range(len(l)):
+    if(i==(l[k])):
+        t+1
+        break
+    if(t==0):
+        if(i==g):
+            print(sep="ln")
+            break
+        else:
+            cl=append(i);
+            print(cl)
