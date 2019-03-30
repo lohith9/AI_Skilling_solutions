1b5f7cf6ca398ff245e8af5c85f584009062019c
LOHITH <35166080+lohith9@users.noreply.github.com>
2019-03-30 22:17:29 +0530

    Create Hill_climb_block.py

---
+++ Hill_climb_block.py
@@ -0,0 +1,38 @@
+stack = ["A", "D", "C", "B"]
+g = ["D", "C", "B", "A"]
+v1 = stack.pop()
+v2 = stack.pop()
+v3 = stack.pop()
+v4 = stack.pop()
+if g[0] == v1:
+    print(v1)
+if g[0] == v2:
+    print(v2)
+if g[0] == v3:
+    print(v3)
+if g[0] == v4:
+    print(v4)
+if g[1] == v1:
+    print(v1)
+if g[1] == v2:
+    print(v2)
+if g[1] == v3:
+    print(v3)
+if g[1] == v4:
+    print(v4)
+if g[2] == v1:
+    print(v1)
+if g[2] == v2:
+    print(v2)
+if g[2] == v3:
+    print(v3)
+if g[2] == v4:
+    print(v4)
+if g[3] == v1:
+    print(v1)
+if g[3] == v2:
+    print(v2)
+if g[3] == v3:
+    print(v3)
+if g[3] == v4:
+    print(v4)
