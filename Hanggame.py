8cc51d7ad61847cdf36ec60851e3f390d44782f7
LOHITH <35166080+lohith9@users.noreply.github.com>
2019-03-30 22:13:54 +0530

    Create Hanggame.py

---
+++ Hanggame.py
@@ -0,0 +1,25 @@
+name =input("What is your name? ")
+print ("Hello, " + name, "Time to play hangman!")
+print ("Start guessing...")
+word = input("Enter the word to guess")
+guesses = ''
+turns = 10
+while turns > 0:         
+    failed = 0             
+    for char in word:      
+        if char in guesses:    
+            print (char,end=" ")   
+        else:
+            print ("_",end=" ")   
+            failed += 1    
+    if failed == 0:                
+         print ("\nYou won")  
+         break              
+    guess = input("guess a character:") 
+    guesses += guess                    
+    if guess not in word:  
+        turns -= 1        
+        print ("Wrong")    
+        print ("You have", + turns, 'more guesses') 
+        if turns == 0:           
+            print ("\nYou Loose")  
